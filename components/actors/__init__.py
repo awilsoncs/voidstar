c6efe9cd5df5e78987c57418a20d2d32c36ca5a0
Aaron Wilson <acwilsoncs@gmail.com>
2021-12-09 08:58:18 -0500

    Fix step intention map

--- components/actors/__init__.py
+++ components/actors/__init__.py
@@ -2,14 +2,10 @@ from components.enums import Intention
 
 VECTOR_STEP_MAP = {
     (0, -1): Intention.STEP_NORTH,
-    (-1, -1): Intention.STEP_NORTH,
-    (1, -1): Intention.STEP_NORTH,
-    (0, 1): Intention.STEP_SOUTH,
-    (0, 0): Intention.NONE,
-    (1, 1): Intention.STEP_SOUTH,
-    (-1, 1): Intention.STEP_SOUTH,
     (1, 0): Intention.STEP_EAST,
     (-1, 0): Intention.STEP_WEST,
+    (0, 1): Intention.STEP_SOUTH,
+    (0, 0): Intention.NONE,
     (1, -1): Intention.STEP_NORTH_EAST,
     (-1, -1): Intention.STEP_NORTH_WEST,
     (1, 1): Intention.STEP_SOUTH_EAST,
