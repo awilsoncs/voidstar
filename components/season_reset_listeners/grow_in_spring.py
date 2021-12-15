19b4ceaa3c649ccf27635c0dd55ccdec5bdc62c9
Aaron Wilson <acwilsoncs@gmail.com>
2021-12-14 18:02:10 -0700

    Saplings only grow in Spring

--- components/season_reset_listeners/grow_in_spring.py
+++ components/season_reset_listeners/grow_in_spring.py
@@ -4,17 +4,19 @@ from components import Coordinates
 from components.actors.calendar_actor import Calendar
 from components.season_reset_listeners.seasonal_actor import SeasonResetListener
 from content.trees import make_tree
+from engine import core
 
 
 @dataclass
 class GrowInSpring(SeasonResetListener):
     def on_season_reset(self, scene):
-        calendar = scene.cm.get(Calendar)
+        calendar = scene.cm.get_one(Calendar, entity=core.get_id("calendar"))
         if not calendar:
             return
 
-        coords = scene.cm.get_one(Coordinates, entity=self.entity)
-        x = coords.x
-        y = coords.y
-        scene.cm.delete(self.entity)
-        scene.cm.add(*make_tree(x, y)[1])
+        if calendar.season == 1:
+            coords = scene.cm.get_one(Coordinates, entity=self.entity)
+            x = coords.x
+            y = coords.y
+            scene.cm.delete(self.entity)
+            scene.cm.add(*make_tree(x, y)[1])
