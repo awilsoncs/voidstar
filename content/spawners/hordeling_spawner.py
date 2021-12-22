acc18fbe1a42b907cafda6385f6b9175e562e413
Aaron Wilson <acwilsoncs@gmail.com>
2021-12-22 08:38:48 -0700

    Add descriptions to spawners

--- content/spawners/hordeling_spawner.py
+++ content/spawners/hordeling_spawner.py
@@ -2,13 +2,15 @@ from components import Entity, Coordinates
 from components.actors.hordeling_spawner import HordelingSpawner
 from engine import core
 
+description = "How did you even see this?"
+
 
 def hordeling_spawner(x, y):
     entity_id = core.get_id()
     return (
         entity_id,
         [
-            Entity(id=entity_id, entity=entity_id, name='hordeling spawner'),
+            Entity(id=entity_id, entity=entity_id, name='hordeling spawner', description=description),
             Coordinates(entity=entity_id, x=x, y=y),
             HordelingSpawner(entity=entity_id)
         ]
--- content/spawners/hordeling_spawner_spawner.py
+++ content/spawners/hordeling_spawner_spawner.py
@@ -2,13 +2,15 @@ from components import Entity
 from components.actors.hordeling_spawner_spawner import HordelingSpawnerSpawner
 from engine import core
 
+description = "How did you even see this?"
+
 
 def hordeling_spawner_spawner(waves):
     entity_id = core.get_id()
     return (
         entity_id,
         [
-            Entity(id=entity_id, entity=entity_id, name='hordeling spawner spawner'),
+            Entity(id=entity_id, entity=entity_id, name='hordeling spawner spawner', description=description),
             HordelingSpawnerSpawner(entity=entity_id, waves=waves)
         ]
     )
