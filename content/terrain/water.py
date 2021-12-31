908cc4bd6705e3062f1ddeb95aa703dce81fb870
Aaron Wilson <acwilsoncs@gmail.com>
2021-12-31 11:27:15 -0700

    Remove water difficult terrain (it's not walkable anyways)

--- content/terrain/water.py
+++ content/terrain/water.py
@@ -2,7 +2,6 @@ from components import Entity, Appearance, Coordinates
 from components.diggable import Diggable
 from components.flooder import Flooder
 from components.material import Material
-from components.states.move_cost_affectors import DifficultTerrain
 from components.pathfinder_cost import PathfinderCost
 from engine import core, palettes
 from engine.constants import PRIORITY_LOWEST
@@ -17,7 +16,6 @@ def make_water(x, y):
             Appearance(entity=entity_id, symbol='~', color=palettes.LIGHT_WATER, bg_color=palettes.BACKGROUND),
             Coordinates(entity=entity_id, x=x, y=y, priority=PRIORITY_LOWEST),
             Material(entity=entity_id, blocks=True, blocks_sight=False),
-            DifficultTerrain(entity=entity_id),
             Diggable(entity=entity_id),
             Flooder(entity=entity_id),
             PathfinderCost(entity=entity_id, cost=0)
