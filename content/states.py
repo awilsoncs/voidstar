13b5d2de4684e84e59576f217bb430a6785a9471
Aaron Wilson <acwilsoncs@gmail.com>
2021-12-31 11:55:38 -0700

    Make alerts appear above FOV

--- content/states.py
+++ content/states.py
@@ -10,7 +10,13 @@ def confused_animation(x, y):
         entity_id,
         [
             Entity(id=entity_id, entity=entity_id, name='confused_anim'),
-            Appearance(entity=entity_id, symbol='?', color=palettes.LIGHT_WATER, bg_color=palettes.BACKGROUND),
+            Appearance(
+                entity=entity_id,
+                symbol='?',
+                color=palettes.LIGHT_WATER,
+                bg_color=palettes.BACKGROUND,
+                render_mode=Appearance.RenderMode.HIGH_VEE
+            ),
             Coordinates(entity=entity_id, x=x, y=y, priority=PRIORITY_HIGH, buildable=True),
             AnimationFloat(entity=entity_id, duration=5),
         ]
@@ -23,7 +29,13 @@ def no_money_animation(x, y):
         entity_id,
         [
             Entity(id=entity_id, entity=entity_id, name='no_money_anim'),
-            Appearance(entity=entity_id, symbol='$', color=palettes.FRESH_BLOOD, bg_color=palettes.BACKGROUND),
+            Appearance(
+                entity=entity_id,
+                symbol='$',
+                color=palettes.FRESH_BLOOD,
+                bg_color=palettes.BACKGROUND,
+                render_mode=Appearance.RenderMode.HIGH_VEE
+            ),
             Coordinates(entity=entity_id, x=x, y=y, priority=PRIORITY_HIGH, buildable=True),
             AnimationFloat(entity=entity_id, duration=5),
         ]
@@ -36,7 +48,13 @@ def help_animation(x, y):
         entity_id,
         [
             Entity(id=entity_id, entity=entity_id, name='help_anim'),
-            Appearance(entity=entity_id, symbol='!', color=palettes.HORDELING, bg_color=palettes.BACKGROUND),
+            Appearance(
+                entity=entity_id,
+                symbol='!',
+                color=palettes.HORDELING,
+                bg_color=palettes.BACKGROUND,
+                render_mode=Appearance.RenderMode.HIGH_VEE
+            ),
             Coordinates(entity=entity_id, x=x, y=y, priority=PRIORITY_HIGH, buildable=True),
             AnimationFloat(entity=entity_id, duration=5),
         ]
@@ -49,7 +67,13 @@ def knockback_animation(x, y):
         entity_id,
         [
             Entity(id=entity_id, entity=entity_id, name='knockback_anim'),
-            Appearance(entity=entity_id, symbol='x', color=palettes.GOLD, bg_color=palettes.BACKGROUND),
+            Appearance(
+                entity=entity_id,
+                symbol='x',
+                color=palettes.GOLD,
+                bg_color=palettes.BACKGROUND,
+                render_mode=Appearance.RenderMode.HIGH_VEE
+            ),
             Coordinates(entity=entity_id, x=x, y=y, priority=PRIORITY_HIGH, buildable=True),
             AnimationFloat(entity=entity_id, duration=5),
         ]
@@ -62,7 +86,13 @@ def cant_shoot_animation(x, y):
         entity_id,
         [
             Entity(id=entity_id, entity=entity_id, name='cant_shoot_anim'),
-            Appearance(entity=entity_id, symbol=')', color=palettes.LIGHT_WATER, bg_color=palettes.BACKGROUND),
+            Appearance(
+                entity=entity_id,
+                symbol=')',
+                color=palettes.LIGHT_WATER,
+                bg_color=palettes.BACKGROUND,
+                render_mode=Appearance.RenderMode.HIGH_VEE
+            ),
             Coordinates(entity=entity_id, x=x, y=y, priority=PRIORITY_HIGH, buildable=True),
             AnimationFloat(entity=entity_id, duration=5),
         ]
