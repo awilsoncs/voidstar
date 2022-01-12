258e80c99bc600f7e1614fa094862b3c24514fad
Aaron Wilson <acwilsoncs@gmail.com>
2022-01-12 08:14:19 -0700

    Refactor debug painters to a base class

--- components/brains/painters/create_gold_actor.py
+++ components/brains/painters/create_gold_actor.py
@@ -1,70 +1,15 @@
 from dataclasses import dataclass
 
-import tcod
-
-import settings
-from components import Coordinates
 from components.actors.energy_actor import EnergyActor
-from components.brains.temporary_brain import TemporaryBrain
-from components.enums import Intention
+from components.brains.painters.painter_brain import PainterBrain
 from content.getables.gold import make_gold_nugget
-from engine import constants, core
+from engine import constants
 
 
 @dataclass
-class PlaceGoldController(TemporaryBrain):
+class PlaceGoldController(PainterBrain):
     energy_cost: int = EnergyActor.INSTANT
     cursor: int = constants.INVALID
 
-    def act(self, scene) -> None:
-        key_event = core.get_key_event()
-        if key_event:
-            key_event = key_event.sym
-            intention = KEY_ACTION_MAP.get(key_event, None)
-            if intention in {
-                Intention.STEP_NORTH,
-                Intention.STEP_EAST,
-                Intention.STEP_WEST,
-                Intention.STEP_SOUTH
-            }:
-                self._move_cursor(scene, intention)
-            if intention is Intention.USE_ABILITY:
-                self._place_gold(scene)
-            elif intention is Intention.BACK:
-                scene.cm.delete(self.cursor)
-                self.back_out(scene)
-
-    def _place_gold(self, scene):
-        coords = scene.cm.get_one(Coordinates, entity=self.cursor)
-        scene.cm.add(*make_gold_nugget(coords.x, coords.y)[1])
-
-    def _move_cursor(self, scene, intention):
-        cursor_coords = scene.cm.get_one(Coordinates, entity=self.cursor)
-        direction = STEP_VECTORS[intention]
-        cursor_coords.x += direction[0]
-        cursor_coords.y += direction[1]
-        if 0 > cursor_coords.x or cursor_coords.x >= settings.MAP_WIDTH:
-            cursor_coords.x -= direction[0]
-        if 0 > cursor_coords.y or cursor_coords.y >= settings.MAP_HEIGHT:
-            cursor_coords.y -= direction[1]
-
-
-KEY_ACTION_MAP = {
-    tcod.event.K_UP: Intention.STEP_NORTH,
-    tcod.event.K_DOWN: Intention.STEP_SOUTH,
-    tcod.event.K_RIGHT: Intention.STEP_EAST,
-    tcod.event.K_LEFT: Intention.STEP_WEST,
-    tcod.event.K_ESCAPE: Intention.BACK,
-    tcod.event.K_SPACE: Intention.USE_ABILITY
-}
-
-STEP_VECTORS = {
-    Intention.STEP_NORTH: (0, -1),
-    Intention.STEP_SOUTH: (0, 1),
-    Intention.STEP_EAST: (1, 0),
-    Intention.STEP_WEST: (-1, 0),
-    Intention.STEP_NORTH_EAST: (1, -1),
-    Intention.STEP_NORTH_WEST: (-1, -1),
-    Intention.STEP_SOUTH_EAST: (1, 1),
-    Intention.STEP_SOUTH_WEST: (-1, 1)
-}
+    def paint_one(self, scene, position):
+        return make_gold_nugget(position[0], position[1])[1]
---
+++ components/brains/painters/painter_brain.py
@@ -0,0 +1,76 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+import tcod
+
+import settings
+from components import Coordinates
+from components.actors.energy_actor import EnergyActor
+from components.brains.temporary_brain import TemporaryBrain
+from components.enums import Intention
+from content.enemies.juvenile import make_juvenile
+from engine import constants, core
+
+
+@dataclass
+class PainterBrain(TemporaryBrain, ABC):
+    """Provide a base class for debug object placing controllers."""
+    energy_cost: int = EnergyActor.INSTANT
+    cursor: int = constants.INVALID
+
+    def act(self, scene) -> None:
+        key_event = core.get_key_event()
+        if key_event:
+            key_event = key_event.sym
+            intention = KEY_ACTION_MAP.get(key_event, None)
+            if intention in {
+                Intention.STEP_NORTH,
+                Intention.STEP_EAST,
+                Intention.STEP_WEST,
+                Intention.STEP_SOUTH
+            }:
+                self._move_cursor(scene, intention)
+            if intention is Intention.USE_ABILITY:
+                self._paint(scene)
+            elif intention is Intention.BACK:
+                scene.cm.delete(self.cursor)
+                self.back_out(scene)
+
+    def _paint(self, scene):
+        coords = scene.cm.get_one(Coordinates, entity=self.cursor)
+        scene.cm.add(*self.paint_one(scene, coords.position))
+
+    @abstractmethod
+    def paint_one(self, scene, position):
+        pass
+
+    def _move_cursor(self, scene, intention):
+        cursor_coords = scene.cm.get_one(Coordinates, entity=self.cursor)
+        direction = STEP_VECTORS[intention]
+        cursor_coords.x += direction[0]
+        cursor_coords.y += direction[1]
+        if 0 > cursor_coords.x or cursor_coords.x >= settings.MAP_WIDTH:
+            cursor_coords.x -= direction[0]
+        if 0 > cursor_coords.y or cursor_coords.y >= settings.MAP_HEIGHT:
+            cursor_coords.y -= direction[1]
+
+
+KEY_ACTION_MAP = {
+    tcod.event.K_UP: Intention.STEP_NORTH,
+    tcod.event.K_DOWN: Intention.STEP_SOUTH,
+    tcod.event.K_RIGHT: Intention.STEP_EAST,
+    tcod.event.K_LEFT: Intention.STEP_WEST,
+    tcod.event.K_ESCAPE: Intention.BACK,
+    tcod.event.K_SPACE: Intention.USE_ABILITY
+}
+
+STEP_VECTORS = {
+    Intention.STEP_NORTH: (0, -1),
+    Intention.STEP_SOUTH: (0, 1),
+    Intention.STEP_EAST: (1, 0),
+    Intention.STEP_WEST: (-1, 0),
+    Intention.STEP_NORTH_EAST: (1, -1),
+    Intention.STEP_NORTH_WEST: (-1, -1),
+    Intention.STEP_SOUTH_EAST: (1, 1),
+    Intention.STEP_SOUTH_WEST: (-1, 1)
+}
