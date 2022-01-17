19899559fe55d11f5ccc7db3a1f2344dec46a2b6
Aaron Wilson <acwilsoncs@gmail.com>
2022-01-17 07:56:42 -0700

    make trees grow daily based on weather

--- components/season_reset_listeners/grow_in_spring.py
+++ components/season_reset_listeners/grow_in_spring.py
@@ -1,24 +1,33 @@
 import logging
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import random
 
 from components import Coordinates
-from components.season_reset_listeners.seasonal_actor import SeasonResetListener
+from components.events.new_day_event import DayBeganListener
 from components.weather.weather import Weather
 from content.terrain.trees import make_tree
 
 
+def temperature_time_to_grow():
+    return random.randint(1200, 3600)
+
+
 @dataclass
-class GrowIntoTree(SeasonResetListener):
-    def on_season_reset(self, scene, season):
+class GrowIntoTree(DayBeganListener):
+    time_to_grow: int = field(default_factory=temperature_time_to_grow)
+
+    def on_new_day(self, scene, day):
         weather = scene.cm.get(Weather)
         if weather:
             weather = weather[0]
         else:
-            logging.warning(f"EID#{self.entity}::GrowIntoTree no weather found")
+            self._log_warning(f"no weather found")
             return
 
-        if random.randint(0, 200) < weather.seasonal_norm:
+        self.time_to_grow = max(0, self.time_to_grow - max(0, weather.temperature))
+
+        if self.time_to_grow <= 0:
+            self._log_debug("sapling growing into a tree")
             coords = scene.cm.get_one(Coordinates, entity=self.entity)
             x = coords.x
             y = coords.y
