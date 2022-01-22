a1f4d721540953c218b4a2110b71572448626a34
Aaron Wilson <acwilsoncs@gmail.com>
2022-01-22 15:08:06 -0700

    Make newly flooded holes dirty if they are adjacent to a dirty source

--- components/flood_nearby_holes.py
+++ components/flood_nearby_holes.py
@@ -5,13 +5,18 @@ from components.base_components.energy_actor import EnergyActor
 from components.floodable import Floodable
 from components.flooder import Flooder
 from components.events.hole_dug_events import HoleDugListener
-from content.terrain.water import make_water
+from components.tags.water_tag import WaterTag
+from components.world_building.world_parameters import WorldParameters
+from content.terrain.water import make_water, make_swampy_water
+from engine import core
 
 
-def _fill_hole(scene, hole):
+def _fill_hole(scene, hole, painter):
+    world_params = scene.cm.get_one(WorldParameters, entity=core.get_id("world"))
+
     coordinates = scene.cm.get_one(Coordinates, entity=hole)
     scene.cm.delete(hole)
-    water = make_water(coordinates.x, coordinates.y)
+    water = painter(coordinates.x, coordinates.y, rapidness=world_params.river_rapids)
     scene.cm.add(*water[1])
 
 
@@ -26,6 +31,7 @@ class FloodHolesSystem(EnergyActor, HoleDugListener):
     is_recharging: bool = False
 
     def on_hole_dug(self, scene):
+        self._log_debug("beginning to fill nearby holes")
         self.is_recharging = True
 
     def act(self, scene) -> None:
@@ -33,17 +39,29 @@ class FloodHolesSystem(EnergyActor, HoleDugListener):
 
     def _fill_step(self, scene):
         # find all floodables with an adjacent flooder
-        floodables = scene.cm.get(
-            Floodable,
-            query=lambda this: any(is_adjacent(scene, this.entity, other.entity) for other in scene.cm.get(Flooder)),
-            project=lambda this: this.entity
-        )
+        floodables = scene.cm.get(Floodable)
+        flooders = scene.cm.get(Flooder)
 
-        if not floodables:
-            self.is_recharging = False
-
+        found = False
+        self._log_debug("attempting to fill")
         for floodable in floodables:
-            _fill_hole(scene, floodable)
+            nearby_flooders = [
+                flooder
+                for flooder in flooders
+                if is_adjacent(scene, floodable.entity, flooder.entity)
+            ]
+
+            if nearby_flooders:
+                found = True
+                if any(scene.cm.get_one(WaterTag, entity=flooder.entity).is_dirty for flooder in nearby_flooders):
+                    _fill_hole(scene, floodable.entity, make_swampy_water)
+                else:
+                    _fill_hole(scene, floodable.entity, make_water)
+
+        if not found:
+            self.is_recharging = False
+            self._log_debug("done filling available floodables")
+
         self.pass_turn(EnergyActor.HALF_HOUR)
 
 
