b5bdc888e0d65df6257400bdcc22ca6f1b55d3f1
Aaron Wilson <acwilsoncs@gmail.com>
2022-02-12 13:48:18 -0700

    Add docstring to DieOnAttackFinished

--- components/die_on_attack_finished.py
+++ components/die_on_attack_finished.py
@@ -6,6 +6,7 @@ from components.events.die_events import Die
 
 @dataclass
 class DieOnAttackFinished(OnAttackFinishedListener):
+    """After this entity attacks, it dies."""
     def on_attack_finished(self, scene, caller):
         if caller == self.entity:
             scene.cm.add(Die(entity=self.entity))
